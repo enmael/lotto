0053843fbacbff67daad7f44760bc9daed3247e5
enmael <enmael25@gmail.com>
2021-10-30 22:07:02 +0900

    Update lotto_controller.rb
    변수의 이름 변경

--- lotto/lotto_controller.rb
+++ lotto/lotto_controller.rb
@@ -1,10 +1,11 @@
 class LottoController < ApplicationController
     def index
-        @bynusu = [*1..45]
-        @lotto1 = @bynusu.sample(6)
-        @lotto2 = @bynusu.sample(6)
-        @lotto3 = @bynusu.sample(6)
-        @lotto4 = @bynusu.sample(6)
+        @number45 = [*1..45]
+        @lotto1 = @number45.sample(6)
+        @lotto2 = @number45.sample(6)
+        @lotto3 = @number45.sample(6)
+        @lotto4 = @number45.sample(6)
+        @lotto5 = @number45.sample(6)
     end
 
 end
