08f2633bf0cfe191f3ed82271dbbe0518ae0605f
enmael <enmael25@gmail.com>
2021-11-01 20:35:10 +0900

    장승배
    html 파일을 이름변경,  html 코드에 대한 대폭 적인 수정(중앙정렬, 버튼 누르면 새로고침 되며 새로운 로또 번호 생성)

--- lotto/lotto_controller.rb
+++ lotto/lotto_controller.rb
@@ -1,20 +1,11 @@
 class LottoController < ApplicationController
-    def index
-
-        @number45 = [*1..45]
-        @lotto1 = @number45.sample(6)
-        @lotto2 = @number45.sample(6)
-        @lotto3 = @number45.sample(6)
-        @lotto4 = @number45.sample(6)
-        @lotto5 = @number45.sample(6)
-
-        @bynusu = [*1..45]
-        @lotto1 = @bynusu.sample(6)
-        @lotto2 = @bynusu.sample(6)
-        @lotto3 = @bynusu.sample(6)
-        @lotto4 = @bynusu.sample(6)
-        @lotto5 = @bynusu.sample(6)
-        
+    def lotto_html   #lolotto_controller 와 lotto_html의 연결해주는 부분
+        number45 = [*1..45]  #1, 45의 배열을 만드는 부분
+        @lotto1 = number45.sample(6).sort  #sample는 number45에서 6개의 값을 랜덤으로 추출한다.
+        @lotto2 = number45.sample(6).sort  #sort는 6개의 숫자를 오름 차순으로 정리한다.
+        @lotto3 = number45.sample(6).sort
+        @lotto4 = number45.sample(6).sort
+        @lotto5 = number45.sample(6).sort
     end
 
 end
--- lotto/routes.rb
+++ lotto/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    get '/lotto23', to: 'lotto#index'
+    get '/lotto23', to: 'lotto#lotto_html'
+    # get '/lotto23' 는 사이트의 주소
+    # to는 'lotto#ll',lotto_html 가져온다.
 end
