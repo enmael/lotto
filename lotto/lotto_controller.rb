147801157f9cd5e4128d2e6097d81024abd42c26
enmael <enmael25@gmail.com>
2021-11-29 21:15:55 +0900

    장승배
    html,css,자바스크립트 코드에 대한 전반적인 수정이 이루어진 대격변급 패치 그로 인해 발생한 사용하지 않는 css class 몇가지가 존재
    
    그래또 다크모드 다음 로또추첨일 까지 수동으로 타임어를 보여줄수있음

--- lotto/lotto_controller.rb
+++ lotto/lotto_controller.rb
@@ -1,11 +1,13 @@
 class LottoController < ApplicationController
     def lotto_html   #lolotto_controller 와 lotto_html의 연결해주는 부분
         number45 = [*1..45]  #1, 45의 배열을 만드는 부분
-        @lotto1 = number45.sample(6).sort  #sample는 number45에서 6개의 값을 랜덤으로 추출한다.
-        @lotto2 = number45.sample(6).sort  #sort는 6개의 숫자를 오름 차순으로 정리한다.
-        @lotto3 = number45.sample(6).sort
-        @lotto4 = number45.sample(6).sort
-        @lotto5 = number45.sample(6).sort
+        @number_winning = [17,18,21,27,29,33,26]
+        @lotto1 = number45.sample(7).sort  #sample는 number45에서 6개의 값을 랜덤으로 추출한다.
+        @lotto2 = number45.sample(7).sort  #sort는 6개의 숫자를 오름 차순으로 정리한다.
+        @lotto3 = number45.sample(7).sort
+        @lotto4 = number45.sample(7).sort
+        @lotto5 = number45.sample(7).sort
     end
 
 end
+
