eb6bd6f74bc43c3c367ac12f71d05f18a4409eee
enmael <enmael25@gmail.com>
2021-12-05 10:57:27 +0900

    장승배
    *수정사항
    - html에 탭 기능을 만들어서 주식창과 로또창을 분활하는데 성공함
    -로또추첨 방식을 뜯어고침

--- lotto/lotto_controller.rb
+++ lotto/lotto_controller.rb
@@ -10,20 +10,172 @@
     *최종수정자: 장승배
     *최종수정내용: 주석의 추가
     --------------------------------------------------------------------------------------
-    *내용: 45가지 숫자중 8개의 숫자를 랜덤으로 생성하여 @lotto에 저장한다.
-    *추가 요구사항 : 로또를 추첨하는 방법을 좀 같다 쓰지말고 만들어서 사용하자
-               예를 들어서 100회분을 로또를 가져와서 거기서 랜덤 하게 뽑는다 든가?
-
+    *내용: 지난 100회차 로또분을 전부 확인하고 계산해본 결과 
+          노랑, 노랑, 빨강, 녹색, 나머지색 이라는 참 으로 신기한 결과가 나왔습니다.
+          그래서 첫번째, 두번째 자리에는 무조건 노랑색 공이 오게 하고 세번째 자리에는 무조건 빨간색이 
+          네번째는 좀 그렇지만 녹색을 다섯번째와 여섯번째의 경우 남은 공들을 랜덤으로 뽑게 하였습니다.
+          확인은 if문으로 일일이 확인하여 비교하였습니다.
+                
 ###########################################################################################
 =end
 class LottoController < ApplicationController
     def lotto_html   #lolotto_controller 와 lotto_html의 연결해주는 부분
-        number45 = [*1..45]  #1, 45의 배열을 만드는 부분
-        @lotto1 = number45.sample(7).sort  #sample는 number45에서 6개의 값을 랜덤으로 추출한다.
-        @lotto2 = number45.sample(7).sort  #sort는 6개의 숫자를 오름 차순으로 정리한다.
-        @lotto3 = number45.sample(7).sort
-        @lotto4 = number45.sample(7).sort
-        @lotto5 = number45.sample(7).sort
+        
+        yellow_ball = [*1..10]                              #노란색 공의 범위는 1~10
+        blue_ball = [*11..20]                               #파란색 공의 범위는 11~20
+        red_ball = [*21..30]                                #빨간색 공의 범위는 21~30
+        black_ball=[*31..40]                                #검은색 공의 범위는 31~40
+        green_ball=[*41..45]                                #초록색 공의 범위는 41~45
+        number3 = [*1..45]                                  #나머지 공의 범위는 1~45
+        
+        
+        @lotto_yellow1 = yellow_ball.sample(2)              #노란공의 값을 두번 뽑는다.
+        @lotto_yellow2 = yellow_ball.sample(2)
+        @lotto_yellow3 = yellow_ball.sample(2)
+        @lotto_yellow4 = yellow_ball.sample(2)
+        @lotto_yellow5 = yellow_ball.sample(2)
+        
+        @lotto_red1 = red_ball.sample(1)                    #빨간 공이 나올 값.
+        @lotto_red2 = red_ball.sample(1)
+        @lotto_red3 = red_ball.sample(1)
+        @lotto_red4 = red_ball.sample(1)
+        @lotto_red5 = red_ball.sample(1)
+        
+        @lotto_green1 = green_ball.sample(1)                #초록 공이 나올 값.
+        @lotto_green2 = green_ball.sample(1)
+        @lotto_green3 = green_ball.sample(1)
+        @lotto_green4 = green_ball.sample(1)
+        @lotto_green5 = green_ball.sample(1)
+        
+        @lotto_number1 = number3.sample(2).sort            #나머지 공이 나올 값들.              
+        @lotto_number2 = number3.sample(2).sort
+        @lotto_number3 = number3.sample(2).sort
+        @lotto_number4 = number3.sample(2).sort
+        @lotto_number5 = number3.sample(2).sort
+        
+        
+        
+        if @lotto_yellow1[0] == @lotto_number1[0]         #첫번째 경우의 수의 반복 제거
+            @lotto_number1[0] = number3.sample[1]
+        
+        elsif @lotto_red1[0]  == @lotto_number1[0]
+            @lotto_number1[0] = number3.sample[1]
+        
+        elsif @lotto_green1[0]  == @lotto_number1[0]
+            @lotto_number1[0] = number3.sample[1]
+        
+        elsif @lotto_yellow1[1] == @lotto_number1[1] 
+            @lotto_number1[1] = number3.sample[1]
+        
+        elsif @lotto_red1[1]  == @lotto_number1[1]
+            @lotto_number1[1] = number3.sample[1]
+        
+        elsif @lotto_green1[1]  == @lotto_number1[1]
+            @lotto_number1[1] = number3.sample[1]
+        
+        elsif @lotto_number1[0] == @lotto_number1[1]
+            @lotto_number1[1] = number3.sample[1]
+            
+        end
+        
+        
+        
+        
+        if @lotto_yellow2[0] == @lotto_number2[0]         #두번째 경우의 수의 반복 제거
+            @lotto_number2[0] = number3.sample[1]
+        
+        elsif @lotto_red2[0]  == @lotto_number2[0]
+            @lotto_number2[0] = number3.sample[1]
+        
+        elsif @lotto_green2[0]  == @lotto_number2[0]
+            @lotto_number2[0] = number3.sample[1]
+        
+        elsif @lotto_yellow2[1] == @lotto_number2[1] 
+            @lotto_number2[1] = number3.sample[1]
+        
+        elsif @lotto_red2[1]  == @lotto_number2[1]
+            @lotto_number2[1] = number3.sample[1]
+        
+        elsif @lotto_green2[1]  == @lotto_number2[1]
+            @lotto_number2[1] = number3.sample[1]
+        
+        elsif @lotto_number2[0] == @lotto_number2[1]
+            @lotto_number2[1] = number3.sample[1]
+            
+        end
+
+        if @lotto_yellow3[0] == @lotto_number3[0]         #세번째 경우의 수의 반복 제거
+            @lotto_number3[0] = number3.sample[1]
+        
+        elsif @lotto_red3[0]  == @lotto_number3[0]
+            @lotto_number3[0] = number3.sample[1]
+        
+        elsif @lotto_green3[0]  == @lotto_number3[0]
+            @lotto_number3[0] = number3.sample[1]
+        
+        elsif @lotto_yellow3[1] == @lotto_number3[1] 
+            @lotto_number3[1] = number3.sample[1]
+        
+        elsif @lotto_red3[1]  == @lotto_number3[1]
+            @lotto_number3[1] = number3.sample[1]
+        
+        elsif @lotto_green3[1]  == @lotto_number3[1]
+            @lotto_number3[1] = number3.sample[1]
+        
+        elsif @lotto_number3[0] == @lotto_number3[1]
+            @lotto_number3[1] = number3.sample[1]
+        end
+        
+        
+        if @lotto_yellow4[0] == @lotto_number4[0]         #네번째 경우의 수의 반복 제거
+            @lotto_number4[0] = number3.sample[1]
+        
+        elsif @lotto_red4[0]  == @lotto_number4[0]
+            @lotto_number4[0] = number3.sample[1]
+        
+        elsif @lotto_green4[0]  == @lotto_number4[0]
+            @lotto_number4[0] = number3.sample[1]
+        
+        elsif @lotto_yellow4[1] == @lotto_number4[1] 
+            @lotto_number4[1] = number3.sample[1]
+        
+        elsif @lotto_red4[1]  == @lotto_number4[1]
+            @lotto_number4[1] = number3.sample[1]
+        
+        elsif @lotto_green4[1]  == @lotto_number4[1]
+            @lotto_number4[1] = number3.sample[1]
+        
+        elsif @lotto_number4[0] == @lotto_number4[1]
+            @lotto_number4[1] = number3.sample[1]
+        end
+        
+        
+        if @lotto_yellow5[0] == @lotto_number5[0]            #다섯번째 경우의 수의 반복 제거
+            @lotto_number5[0] = number3.sample[1]
+        
+        elsif @lotto_red5[0]  == @lotto_number5[0]
+            @lotto_number5[0] = number3.sample[1]
+        
+        elsif @lotto_green5[0]  == @lotto_number5[0]
+            @lotto_number5[0] = number3.sample[1]
+        
+        elsif @lotto_yellow5[1] == @lotto_number5[1] 
+            @lotto_number5[1] = number3.sample[1]
+        
+        elsif @lotto_red5[1]  == @lotto_number5[1]
+            @lotto_number5[1] = number3.sample[1]
+        
+        elsif @lotto_green5[1]  == @lotto_number5[1]
+            @lotto_number5[1] = number3.sample[1]
+        
+        elsif @lotto_number5[0] == @lotto_number5[1]
+            @lotto_number5[1] = number3.sample[1]
+            
+        end
+
+
+
+        
     end
 
 end
